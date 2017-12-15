3a0f2fc8a457b0c471732f5950cad972e65f9395
alywaly13 <tzhan@mit.edu>
2017-12-15 18:54:09 -0500

    Add android dataset stuff

--- train_model1.py
+++ train_model1.py
@@ -5,7 +5,7 @@
 
 
 import read_input
-from read_input import TrainQuestionDataset, EvalQuestionDataset
+from read_input import TrainQuestionDataset, EvalQuestionDataset, read_word_embeddings
 from prettytable import PrettyTable
 
 from evaluation import Evaluation
@@ -17,104 +17,9 @@ from tqdm import tqdm
 from torch import nn
 import numpy as np
 import os
+import sys
 
-
-# In[ ]:
-
-
-class LSTM(nn.Module):
-    def __init__(self, embeddings, padding_idx, hidden_dim, num_layers, truncate_length, dropout=0.0, bidirectional=False):
-        super(LSTM, self).__init__()
-        if bidirectional:
-            print('Bidirectinoal LSTM not implemented!!!')
-            assert(bidirectional==False)
-        self.num_layers = num_layers
-        self.hidden_dim = hidden_dim
-        self.truncate_length= truncate_length
-        vocab_size, embed_dim = embeddings.shape
-        self.embedding_layer = nn.Embedding(vocab_size, embed_dim, padding_idx=padding_idx)
-        self.embedding_layer.weight.data = torch.from_numpy(embeddings)
-        self.embedding_layer.weight.requires_grad = False # Freezes the word vectors so we don't train them
-        # The LSTM takes word vectors as inputs, and outputs hidden states
-        # with dimensionality hidden_dim.
-        self.lstm = nn.LSTM(embed_dim, hidden_dim, num_layers, batch_first=True, bidirectional=bidirectional)
-        self.dropout = nn.Dropout(p=dropout)
-        #self.hidden = self.init_hidden()
-
-    def init_hidden(self, batch_size):
-        # Before we've done anything, we dont have any hidden state.
-        # The axes semantics are (num_layers, minibatch_size, hidden_dim)
-        return (Variable(torch.zeros(self.num_layers, batch_size, self.hidden_dim)),
-                Variable(torch.zeros(self.num_layers, batch_size, self.hidden_dim)))
-
-    def forward(self, sentence_inp, mask):
-        # sentence_inp - batch_size x truncate_length
-        # mask - batch_size x truncate_length
-        batch_size = sentence_inp.size()[0]
-        self.hidden = self.init_hidden(batch_size)
-        sentence_vectorized = self.embedding_layer(sentence_inp).float()
-        # lstm expects batch_size x truncate_length x num_features because of batch_first=True
-        outputs_pre_dropout, self.hidden = self.lstm(sentence_vectorized)
-        outputs = self.dropout(outputs_pre_dropout)
-        out_masked = torch.mul(outputs, mask.unsqueeze(2).expand_as(outputs))
-        out_masked_avg = torch.div(out_masked.sum(dim=1), 
-                                   mask.sum(dim=1).unsqueeze(1).expand(batch_size, self.hidden_dim))
-        return out_masked_avg
-
-
-# In[ ]:
-
-
-class CNN(nn.Module):
-    def __init__(self, input_dim, hidden_dim, truncate_length):
-        super(CNN, self).__init__()
-
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.truncate_length= truncate_length
-
-        self.conv = nn.Conv1d(input_dim, hidden_dim, 3, padding=1)
-        self.tanh = nn.Tanh()
-        self.drop = nn.Dropout(p=0.2)
-
-    def forward(self, sentence_inp):
-        # sentence_inp is batch_size x truncate_length x num_features
-        outputs = self.conv(sentence_inp)
-        outputs = self.tanh(outputs)
-        outputs = self.drop(outputs)
-        out_masked = torch.mul(outputs, mask.unsqueeze(2).expand_as(outputs))
-        out_masked_avg = torch.div(out_masked.sum(dim=1), 
-                                   mask.sum(dim=1).unsqueeze(1).expand(batch_size, self.hidden_dim))
-        return out_masked_avg
-
-
-# In[ ]:
-
-
-# batch_size = BATCH_SIZE
-# data_loader = torch.utils.data.DataLoader(dev_dataset, batch_size=batch_size,
-#                                               shuffle=True, drop_last=True)
-# for batch in tqdm(data_loader):
-#     q_body = Variable(batch["q_body"], requires_grad=False) # batch_size x truncate_length x 200]
-#     cand_bodies = Variable(batch["candidate_bodies"], requires_grad=False) # batch_size x num_cands x truncate_length x 200
-#     q_title = Variable(batch["q_title"], requires_grad=False)
-#     cand_titles = Variable(batch["candidate_titles"], requires_grad=False)
-#     num_cands = cand_titles.size()[1]
-#     q_body_mask, q_title_mask = Variable(batch["q_body_mask"]), Variable(batch["q_title_mask"])
-#     cand_body_masks, cand_title_masks = Variable(batch["candidate_body_masks"]), Variable(batch["candidate_title_masks"])
-#     break
-
-
-# In[ ]:
-
-
-def evaluate(all_ranked_labels):
-    evaluator = Evaluation(all_ranked_labels)
-    MAP = evaluator.MAP()*100
-    MRR = evaluator.MRR()*100
-    P1 = evaluator.Precision(1)*100
-    P5 = evaluator.Precision(5)*100
-    return MAP, MRR, P1, P5
+from models import LSTM, CNN, evaluate, DomainClassifier
 
 
 # In[ ]:
@@ -129,11 +34,10 @@ def run_epoch(dataset, is_training, model, optimizer, batch_size, margin, save_p
         model.train()
     else:
         model.eval()
-
     requires_grad = False
     for batch in tqdm(data_loader):
-        q_body = Variable(batch["q_body"], requires_grad=requires_grad) # batch_size x truncate_length x 200]
-        cand_bodies = Variable(batch["candidate_bodies"], requires_grad=requires_grad) # batch_size x num_cands x truncate_length x 200
+        q_body = Variable(batch["q_body"], requires_grad=requires_grad) # batch_size x truncate_length
+        cand_bodies = Variable(batch["candidate_bodies"], requires_grad=requires_grad) # batch_size x num_cands x truncate_length
         q_title = Variable(batch["q_title"], requires_grad=requires_grad)
         cand_titles = Variable(batch["candidate_titles"], requires_grad=requires_grad)
         q_body_mask = Variable(batch["q_body_mask"], requires_grad=requires_grad) # batch_size x truncate_length
@@ -150,11 +54,12 @@ def run_epoch(dataset, is_training, model, optimizer, batch_size, margin, save_p
                                 cand_title_masks.view(batch_size*num_cands, TRUNCATE_LENGTH))
         q_enc = q_title_enc + q_body_enc / 2.0
         candidate_encs = cand_title_encs + cand_body_encs / 2.0
+        enc_length = q_enc.size()[-1]
         #domain_predictions = domain_classifier(q_enc, candidate_ends)
         #loss(domain_predictions, target_predictions)
         #domain_optimizer.step()
         candidate_encs = candidate_encs.view(batch_size, num_cands, -1) # batch_size x num_cands x enc_length
-        query_encs = q_enc.view(batch_size, 1, -1).repeat(1, num_cands, 1) # batch_size x (num_cands) x enc_length
+        query_encs = q_enc.view(batch_size, 1, -1).expand_as(candidate_encs) # batch_size x (num_cands) x enc_length
         cos = torch.nn.CosineSimilarity(dim=2, eps=1e-08)(candidate_encs, query_encs) # batch_size x (num_cands)
         
         if is_training:
@@ -210,10 +115,13 @@ def train_model(train_data, dev_data, test_data, model, save_dir=None, batch_siz
                             [ "%.2f" % x for x in [train_loss] + [ dev_MAP, dev_MRR, dev_P1, dev_P5 ] +
                                         [ test_MAP, test_MRR, test_P1, test_P5 ] ])
         print("{}".format(result_table))
+        sys.stdout.flush()
 
 
 # In[ ]:
 
+
+WORD_EMBEDDINGS_FILE = 'askubuntu/vector/vectors_pruned.200.txt'
 
 TEXT_TOKENIZED_FILE = 'askubuntu/text_tokenized.txt'
 TRAIN_FILE = 'askubuntu/train_random.txt'
@@ -221,14 +129,15 @@ DEV_FILE = 'askubuntu/dev.txt'
 TEST_FILE = 'askubuntu/test.txt'
 
 TRUNCATE_LENGTH = 100
+word_to_idx, embeddings, padding_idx = read_input.read_word_embeddings(WORD_EMBEDDINGS_FILE)
 
 
 # In[ ]:
 
 
-train_dataset = TrainQuestionDataset(TEXT_TOKENIZED_FILE, TRAIN_FILE, truncate=TRUNCATE_LENGTH, test_subset=9)
-dev_dataset = EvalQuestionDataset(train_dataset.id_to_question, DEV_FILE, truncate=TRUNCATE_LENGTH, test_subset=9)
-test_dataset = EvalQuestionDataset(train_dataset.id_to_question, TEST_FILE, truncate=TRUNCATE_LENGTH, test_subset=9)
+train_dataset = TrainQuestionDataset(TEXT_TOKENIZED_FILE, TRAIN_FILE, word_to_idx, padding_idx, truncate=TRUNCATE_LENGTH, test_subset=1000)
+dev_dataset = EvalQuestionDataset(train_dataset.id_to_question, DEV_FILE, word_to_idx, padding_idx, truncate=TRUNCATE_LENGTH, test_subset=1000)
+test_dataset = EvalQuestionDataset(train_dataset.id_to_question, TEST_FILE, word_to_idx, padding_idx, truncate=TRUNCATE_LENGTH, test_subset=1000)
 
 
 # In[ ]:
@@ -238,8 +147,6 @@ DROPOUT_PROBS = [0.0, 0.1, 0.2, 0.3] # Taken from paper
 DROPOUT = 0.1
 BIDIRECTIONAL = False
 
-embeddings = read_input.embeddings
-padding_idx = read_input.padding_idx
 model = LSTM(embeddings, padding_idx, 15, 1, TRUNCATE_LENGTH, DROPOUT, BIDIRECTIONAL)
 # Example of how to load a previously trained model
 # model.load_state_dict(torch.load('lstm_saved_models/epoch1.pkl'))
@@ -248,8 +155,8 @@ model = LSTM(embeddings, padding_idx, 15, 1, TRUNCATE_LENGTH, DROPOUT, BIDIRECTI
 # In[ ]:
 
 
-BATCH_SIZE = 3
-NUM_EPOCHS = 2
+BATCH_SIZE = 20
+NUM_EPOCHS = 40
 MARGINS = [0.2, 0.4, 0.6] # Some student on piazza said 0.2 worked really well
 MARGIN = 0.2
 LRS = [1e-3, 3e-4] # Taken from paper
@@ -260,4 +167,10 @@ SAVE_DIR = 'lstm_saved_models'
 train_model(train_dataset, dev_dataset, test_dataset, model, SAVE_DIR,
             num_epochs=NUM_EPOCHS, 
             margin=MARGIN, batch_size=BATCH_SIZE, lr=LR)
+
+
+# In[ ]:
+
+
+# TODO train CNN
 
